e31954142f13c0c4a31b2535c867dbb5689ecbaa
Sumit Bhade <imsumitbhade@gmail.com>
2024-12-25 15:42:23 +0530

    Preparing another problem

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,9 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  //(Problem) Even after selecting no it deletes the place.
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
